6153a213698f048e32388095f4eef5348a3b603c
Hyrumthehotscott <90939615+Hyrumthehotscott@users.noreply.github.com>
2021-12-11 10:43:29 -0800

    Fixed Climber Command

--- src/main/java/frc/robot/commands/MoveClimber.java
+++ src/main/java/frc/robot/commands/MoveClimber.java
@@ -33,6 +33,7 @@ public class MoveClimber extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    climber.setClimbSpeed(0.0);
   }
 
   // Returns true when the command should end.
